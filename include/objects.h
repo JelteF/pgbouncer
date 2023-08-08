5e46b1f49285d1fdc0aaf0fa4e83e90dd2b0ac29
Jelte Fennema <jelte.fennema@microsoft.com>
2023-08-08 13:45:33 +0200

    Revert "Make varcache dynamically configurable. (#867)"
    This reverts commit 8c18fc4d213ad425841434767b71616dc3386102.

--- include/objects.h
+++ include/objects.h
@@ -32,7 +32,6 @@ extern struct Slab *peer_pool_cache;
 extern struct Slab *pool_cache;
 extern struct Slab *user_cache;
 extern struct Slab *iobuf_cache;
-extern struct Slab *var_list_cache;
 
 PgDatabase *find_peer(int peer_id);
 PgDatabase *find_database(const char *name);
